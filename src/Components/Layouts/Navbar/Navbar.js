a2a043f0760b24079b0fbc929e6118ae6642c0a0
wisnupramoedya <wisnupramoedya@gmail.com>
2020-11-06 23:47:04 +0700

    Updating home carousel

--- src/Components/Layouts/Navbar/Navbar.js
+++ src/Components/Layouts/Navbar/Navbar.js
@@ -1,62 +1,70 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
 import TeraskosLogo from './../../../Assets/Images/teraskos_logo.png';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 function Header() {
-    return (
-        <Fragment>
-            <header id="topnav" class="defaultscroll sticky">
-                <div class="container">
-                    <div>
-                        <a class="logo" href="index.html">
-                            <img src={TeraskosLogo} height="72" alt="" />
-                        </a>
-                    </div>
-                    <div class="buy-button">
-                        <a href="#join" class="btn btn-primary">Cari Kos</a>
-                    </div>
+  return (
+    <Fragment>
+      <header id='topnav' class='defaultscroll nav-sticky'>
+        <div class='container'>
+          <div>
+            <a class='logo' href='index.html'>
+              <img src={TeraskosLogo} height='72' alt='' />
+            </a>
+          </div>
+          <div class='buy-button'>
+            <a href='#join' class='btn btn-primary'>
+              Cari Kos
+            </a>
+          </div>
 
-                    <div class="menu-extras">
-                        <div class="menu-item">
-                            <a class="navbar-toggle">
-                                <div class="lines">
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-
-                    <div id="navigation">
-                        <ul class="navigation-menu">
-                            <li>
-                                <Link to={"/"}>Home</Link>
-                            </li>
-                            <li>
-                                <Link to={"/about"}>About Us</Link>
-                            </li>
-                            <li>
-                                <Link to={"/services"}>Services</Link>
-                            </li>
-                            <li>
-                                <Link to={"/blog"}>Blog</Link>
-                            </li>
-                            <li>
-                                <Link to={"/contact"}>Contact</Link>
-                            </li>
-                            <li>
-                                <Link to={"/faq"}>FAQ</Link>
-                            </li>
-                        </ul>
-                        <div class="buy-menu-btn d-none">
-                            <a href="https://1.envato.market/4n73n" target="_blank" class="btn btn-primary">Buy Now</a>
-                        </div>
-                    </div>
+          <div class='menu-extras'>
+            <div class='menu-item'>
+              <a class='navbar-toggle'>
+                <div class='lines'>
+                  <span></span>
+                  <span></span>
+                  <span></span>
                 </div>
-            </header>
-        </Fragment>
-    )
+              </a>
+            </div>
+          </div>
+
+          <div id='navigation'>
+            <ul class='navigation-menu'>
+              <li>
+                <Link to={'/'}>Home</Link>
+              </li>
+              <li>
+                <Link to={'/about'}>About Us</Link>
+              </li>
+              <li>
+                <Link to={'/services'}>Services</Link>
+              </li>
+              <li>
+                <Link to={'/blog'}>Blog</Link>
+              </li>
+              <li>
+                <Link to={'/contact'}>Contact</Link>
+              </li>
+              <li>
+                <Link to={'/faq'}>FAQ</Link>
+              </li>
+            </ul>
+            <div class='buy-menu-btn d-none'>
+              <a
+                href='https://1.envato.market/4n73n'
+                target='_blank'
+                class='btn btn-primary'
+              >
+                Buy Now
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
+    </Fragment>
+  );
 }
 
-export default Header
+export default Header;
--- src/Components/Pages/Index/Components/Header.js
+++ src/Components/Pages/Index/Components/Header.js
@@ -1,18 +1,20 @@
-import React, { Fragment } from 'react'
-import ImageHeader from './../../../../Assets/Images/illustrator/Startup_SVG.svg';
+import React, { Fragment } from 'react';
+// import ImageHeader from './../../../../Assets/Images/illustrator/Startup_SVG.svg';
+import logo from '../../../../logo.svg';
+import './style.scss';
 
 function Header() {
-    return (
-        <Fragment>
-            <section class="bg-half-170  d-table w-100" id="home">
+  return (
+    <Fragment>
+      {/* <section class="bg-half-170  d-table w-100" id="home">
                 <div class="container">
                     <div class="row align-items-center">
                         <div class="col-lg-7 col-md-7">
                             <div class="title-heading mt-4">
                                 <h1 class="heading mb-3">Temukan Segala Kebutuhan Ngekosmu dengan <span class="text-primary">Mudah </span>dan <span class="text-primary">Murah</span>! </h1>
-                                {/* <p class="para-desc text-muted">
+                                <p class="para-desc text-muted">
                                     
-                                </p> */}
+                                </p>
                                 <div class="mt-4">
                                     <a href="page-contact-one.html" class="btn btn-primary mt-2 mr-2">Cari Kos</a>
                                     <a href="https://docs.google.com/forms/d/e/1FAIpQLSc4W4pvH43yohJdXSBFUlKX_M8VEZAUyxOeedf6HkDXtQYv0Q/viewform" class="btn btn-outline-primary mt-2">Gabung Sebagai Mitra Kos</a>
@@ -25,9 +27,19 @@ function Header() {
                         </div>
                     </div>
                 </div>
-            </section>
-        </Fragment>
-    )
+            </section> */}
+      {/* Belum responsive */}
+      <header
+        class='header-bg bg-half-170 d-table w-100 align-items-center justify-content-center'
+        id='home'
+      >
+        <div class='title text-center text-white'>
+          <img src={logo} alt='Logo Teraskos' />
+          <h1>Temukan Segala Kebutuhan Ngekosmu dengan Mudah dan Murah!</h1>
+        </div>
+      </header>
+    </Fragment>
+  );
 }
 
-export default Header
+export default Header;
